937ac8b7b5fb3d6d6bd5dbb9eb4020c4b444247f
Sebastian Mora <seb@ruse.tech>
2021-11-26 23:29:45 -0800

    moving sns to sls

--- terraform/paramters.tf
+++ terraform/paramters.tf
@@ -1,12 +1,6 @@
 
 # Stores the resource ARNS required by SLS
 
-resource "aws_ssm_parameter" "sns-arn" {
-  name        = aws_sns_topic.mc-updates.name
-  description = "Stored the ARN of the SNS topic"
-  type        = "SecureString"
-  value       = "${aws_sns_topic.mc-updates.arn}"
-}
 
 resource "aws_ssm_parameter" "dynamodb-mc" {
   name = aws_dynamodb_table.mc-table.name
--- terraform/sns.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_sns_topic" "mc-updates" {
-  name = "mc-updates"
-}
-
-resource "aws_sns_topic_subscription" "topic_lambda" {
-  topic_arn = aws_sns_topic.mc-updates.arn
-  protocol  = "lambda"
-  endpoint  = var.alert-lambda-arn
-}
