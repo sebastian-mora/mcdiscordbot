b5a43733518ada035ed6250907dc94cd3e1d5fc4
Sebastian Mora <seb@ruse.tech>
2021-11-26 22:29:40 -0800

    adding tf files

---
+++ terraform/db.tf
@@ -0,0 +1,11 @@
+resource "aws_dynamodb_table" "mc-dynamodb-table" {
+  name           = "mcdataa"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "username"
+
+  attribute {
+    name = "username"
+    type = "S"
+  }
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "vpc_cidr" {
+    description = "CIDR for the whole VPC"
+    default = "172.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "172.0.10.0/24"
+}
+
+variable "alert-lambda-arn" {
+  default = "arn:aws:lambda:us-west-2:621056530958:function:mcdiscordbot-dev-sendDiscordMessage"
+}
