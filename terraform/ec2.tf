d97bbb4252c29744805fae8542a6334f17e70dd3
Sebastian Mora <seb@ruse.tech>
2021-11-28 11:11:55 -0800

    adding ec2 subnet ref

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "mc1" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "m4.large"
   key_name                    = aws_key_pair.deployer.key_name
-  subnet_id                   = aws_subnet.mc_public.id
+  subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
   user_data                   = file("resources/scripts/install.sh")
   iam_instance_profile        = aws_iam_instance_profile.test_profile.name
