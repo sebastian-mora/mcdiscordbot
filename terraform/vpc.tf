8bd5295502689398df0c28d9b489f095ee001b8e
Sebastian Mora <seb@ruse.tech>
2021-11-28 11:20:36 -0800

    adding proper azs

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,5 +4,6 @@ module "vpc" {
   name = "mc-vpc"
   cidr = var.vpc_cidr
 
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"]
   public_subnets  = [var.public_subnet_cidr]
 }
