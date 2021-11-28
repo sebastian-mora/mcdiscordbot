7164e7c6c0176a9e0b01e8a4e0cac6f61d33def4
Sebastian Mora <seb@ruse.tech>
2021-11-28 10:40:11 -0800

    fixing subnet cidr

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "mc_vpc" {
 
 resource "aws_subnet" "mc_public" {
   vpc_id                  = aws_vpc.mc_vpc.id
-  cidr_block              = var.vpc_cidr
+  cidr_block              = var.public_subnet_cidr
   map_public_ip_on_launch = true
 }
 # Create the Route Table
