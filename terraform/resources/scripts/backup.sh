d5bd2de35ea3d419f63dc600802f8d0ebe109315
Sebastian Mora <seb@ruse.tech>
2021-11-29 18:31:53 -0800

    updating bak filename

--- terraform/resources/scripts/backup.sh
+++ terraform/resources/scripts/backup.sh
@@ -10,8 +10,7 @@ AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 EC2_NAME=$(aws ec2 describe-tags --region us-west-2 --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
 
 #! /bin/bash
-NOW=$(date +%d%m%Y-%H.%M.%S)
-filename=mc.$EC2_NAME.$NOW.zip
+filename=mc.$EC2_NAME.zip
 
 zip -r /home/ubuntu/server/$filename /home/ubuntu/server/world
 aws s3 mv /home/ubuntu/server/$filename s3://$BUCKET
