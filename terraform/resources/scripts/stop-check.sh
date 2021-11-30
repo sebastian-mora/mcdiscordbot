c4ca1d4daaf926a1b19681058c0270ccacb56dde
Sebastian Mora <seb@ruse.tech>
2021-11-29 20:25:41 -0800

    updating to dynaminc SNS ref

--- terraform/resources/scripts/stop-check.sh
+++ terraform/resources/scripts/stop-check.sh
@@ -3,11 +3,12 @@ PLAYERCOUNT=$(/home/ubuntu/tools/mcrcon/mcrcon -H 127.0.0.1 -P 25575 -p test "li
 AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 EC2_NAME=$(aws ec2 describe-tags --region us-west-2 --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
 echo "Player count: $PLAYERCOUNT"
+SNS_TOPIC=$(aws ssm get-parameter --name "alert-sns" | jq -r '.Parameter.Value')
 
 if [ "$PLAYERCOUNT" -eq "0" ]; then
 
 	sudo systemctl stop minecraft.service
-	aws sns publish --region us-west-2 --topic-arn arn:aws:sns:us-west-2:621056530958:minecraft-alert --message "Shutting down ${EC2_NAME} due to inactivity."
+	aws sns publish --region us-west-2 --topic-arn "${SNS_TOPIC}" --message "Shutting down ${EC2_NAME} due to inactivity."
 	sudo shutdown -h now
 	echo "Shutting down ${EC2_NAME} due to inactivity."
 else
