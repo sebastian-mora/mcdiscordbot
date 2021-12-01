91b8f66405aeb43389f4b80e47e9db147d92ee80
seb <seb@ruse.tech>
2021-11-30 18:02:18 -0800

    bumping server version

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -24,5 +24,5 @@ variable "rconpass" {
 
 variable "mc_server_url" {
   description = "JAR Url for mc version to download"
-  default = "https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar"
+  default = "https://serverjars.com/api/fetchJar/vanilla/1.18"
 }
