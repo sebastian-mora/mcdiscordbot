927a1f8aa7661a6730c0d10f9056e298ec978b67
seb <seb@ruse.tech>
2021-11-30 20:11:33 -0800

    updating path

--- terraform/resources/scripts/stop-check.sh
+++ terraform/resources/scripts/stop-check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PLAYERCOUNT=$(/home/ubuntu/tools/mcrcon/mcrcon -H 127.0.0.1 -P 25575 -p test "list" | cut -d ' ' -f 3)
+PLAYERCOUNT=$(mcrcon -H 127.0.0.1 -P 25575 -p test "list" | cut -d ' ' -f 3)
 AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 EC2_NAME=$(aws ec2 describe-tags --region us-west-2 --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
 echo "Player count: $PLAYERCOUNT"
@@ -7,10 +7,10 @@ SNS_TOPIC=$(aws ssm get-parameter --name "/mc/alert-sns" --with-decryption --reg
 
 if [ "$PLAYERCOUNT" -eq "0" ]; then
 
-	sudo systemctl stop minecraft.service
-	aws sns publish --region us-west-2 --topic-arn "${SNS_TOPIC}" --message "Shutting down ${EC2_NAME} due to inactivity."
-	sudo shutdown -h now
-	echo "Shutting down ${EC2_NAME} due to inactivity."
+        sudo systemctl stop minecraft.service
+        aws sns publish --region us-west-2 --topic-arn "${SNS_TOPIC}" --message "Shutting down ${EC2_NAME} due to inactivity."
+        sudo shutdown -h now
+        echo "Shutting down ${EC2_NAME} due to inactivity."
 else
-	echo "No shuttoff ${EC2_NAME}"
+        echo "No shuttoff ${EC2_NAME}"
 fi
