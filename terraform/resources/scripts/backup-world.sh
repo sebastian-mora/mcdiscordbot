d46bede3984dc273bba8137cce83f8e4e556ff5b
Sebastian Mora <seb@ruse.tech>
2021-12-08 00:07:45 -0800

    updating region param

--- terraform/resources/scripts/backup-world.sh
+++ terraform/resources/scripts/backup-world.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/)
+AZ=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r)
 
 get_param() {
     P=$(/usr/local/bin/aws ssm get-parameter --with-decryption --name  "$1" | jq -r '.Parameter.Value')
@@ -9,7 +9,7 @@ get_param() {
 
 BUCKET=$(get_param "/mc/backup-bucket")
 AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-EC2_NAME=$(/usr/local/bin/aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
+EC2_NAME=$(/usr/local/bin/aws ec2 describe-tags --region $AZ --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
 
 # Save MC World
 /usr/local/bin/mcrcon -p test "say Saving world...." save-all
--- terraform/resources/scripts/stop-check.sh
+++ terraform/resources/scripts/stop-check.sh
@@ -3,7 +3,7 @@ PLAYERCOUNT=$(/usr/local/bin/mcrcon -H 127.0.0.1 -P 25575 -p test "list" | cut -
 AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 EC2_NAME=$(/usr/local/bin/aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
 SNS_TOPIC=$(/usr/local/bin/aws ssm get-parameter --name "/mc/alert-sns" --with-decryption --region ${AZ::-1}  | jq -r '.Parameter.Value')
-AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/)
+AZ=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r)
 
 echo "Player count: $PLAYERCOUNT"
 if [ "$PLAYERCOUNT" -eq "0" ]; then
@@ -12,7 +12,7 @@ if [ "$PLAYERCOUNT" -eq "0" ]; then
 	/home/ubuntu/scripts/backup-world.sh
 	/usr/local/bin/mcrcon -p test "say Stoping the server...." stop
 
-	/usr/local/bin/aws sns publish --region ${AZ::-1} --topic-arn "${SNS_TOPIC}" --message "Shutting down ${EC2_NAME} due to inactivity."
+	/usr/local/bin/aws sns publish --region $AZ --topic-arn "${SNS_TOPIC}" --message "Shutting down ${EC2_NAME} due to inactivity."
 
 	sudo shutdown -h now
 	echo "Shutting down ${EC2_NAME} due to inactivity."
