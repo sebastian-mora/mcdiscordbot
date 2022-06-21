4ac55fb22b4ef09707b0ad3f0cef209a995337ac
seb <seb@ruse.tech>
2022-06-20 23:38:36 -0700

    refactor rcon password

--- terraform/resources/scripts/backup-world.sh
+++ terraform/resources/scripts/backup-world.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 AZ=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r)
+RCON_PASS=$( /usr/local/bin/aws ssm get-parameter --name "/mc/rcon-pass" --with-decryption --region ${AZ}  | jq -r '.Parameter.Value')
 
 get_param() {
     P=$(/usr/local/bin/aws ssm get-parameter --with-decryption --name  "$1" | jq -r '.Parameter.Value')
@@ -12,10 +13,10 @@ AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 EC2_NAME=$(/usr/local/bin/aws ec2 describe-tags --region $AZ --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
 
 # Save MC World
-/usr/local/bin/mcrcon -p test "say Saving world...." save-all
+/usr/local/bin/mcrcon -p $RCON_PASS "say Saving world...." save-all
 filename=mc.$EC2_NAME.zip
 
 (cd /home/ubuntu/server && zip -r - world/) > $filename
 /usr/local/bin/aws s3 mv $filename s3://$BUCKET/worlds/
 
-/usr/local/bin/mcrcon -p test "say Backup saved to S3 bucket"
+/usr/local/bin/mcrcon -p $RCON_PASS "say Backup saved to S3 bucket"
--- terraform/resources/scripts/stop-check.sh
+++ terraform/resources/scripts/stop-check.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 AZ=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r)
-PLAYERCOUNT=$(/usr/local/bin/mcrcon -H 127.0.0.1 -P 25575 -p test "list" | cut -d ' ' -f 3)
+RCON_PASS=$( /usr/local/bin/aws ssm get-parameter --name "/mc/rcon-pass" --with-decryption --region ${AZ}  | jq -r '.Parameter.Value')
 AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 EC2_NAME=$(/usr/local/bin/aws ec2 describe-tags --region $AZ --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
 SNS_TOPIC=$(/usr/local/bin/aws ssm get-parameter --name "/mc/alert-sns" --with-decryption --region $AZ  | jq -r '.Parameter.Value')
 
+PLAYERCOUNT=$(/usr/local/bin/mcrcon -H 127.0.0.1 -P 25575 -p $RCON_PASS "list" | cut -d ' ' -f 3)
 echo "Player count: $PLAYERCOUNT"
 if [ "$PLAYERCOUNT" -eq "0" ]; then
 
         # Run save and backup
         /home/ubuntu/scripts/backup-world.sh
-        /usr/local/bin/mcrcon -p test "say Stoping the server...." stop
+        /usr/local/bin/mcrcon -p $RCON_PASS "say Stoping the server...." stop
 
         /usr/local/bin/aws sns publish --region $AZ --topic-arn "${SNS_TOPIC}" --message "Shutting down ${EC2_NAME} due to inactivity."
 
