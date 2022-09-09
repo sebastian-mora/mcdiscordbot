3223dc684e53d8ff218a259b0b0a55b5f1b6f4db
seb <seb@ruse.tech>
2022-09-09 14:17:32 -0700

    fmt

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,7 +3,7 @@ module "vanilla" {
   source             = "./modules/ec2"
   name               = "vanilla"
   ansible_host_name  = "vanilla"
-  instance_type = "m5.large"
+  instance_type      = "m5.large"
   instance_role_arn  = aws_iam_role.minecraft_server_role.name
   subnet_id          = module.vpc.public_subnets[0]
   security_group_ids = [aws_security_group.mc_sg.id, aws_security_group.allow_ssh_public.id]
