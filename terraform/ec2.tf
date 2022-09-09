3e43eb32bd90fc7d812a95a800d45579add6e69b
seb <seb@ruse.tech>
2022-09-09 14:18:43 -0700

    fix name

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -4,7 +4,7 @@ module "vanilla" {
   name               = "vanilla"
   ansible_host_name  = "vanilla"
   instance_type      = "m5.large"
-  instance_role_arn  = aws_iam_role.minecraft_server_role.name
+  instance_profile_name  = aws_iam_role.minecraft_server_role.name
   subnet_id          = module.vpc.public_subnets[0]
   security_group_ids = [aws_security_group.mc_sg.id, aws_security_group.allow_ssh_public.id]
 }
