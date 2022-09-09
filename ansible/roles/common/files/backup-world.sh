f15ca2d26e5053f88e95ae18ffc3275688742420
seb <seb@ruse.tech>
2022-09-09 00:27:54 -0700

    init roles

---
+++ ansible/roles/common/files/backup-world.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+AZ=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r)
+
+get_param() {
+    P=$(/usr/local/bin/aws ssm get-parameter --with-decryption --name  "$1" | jq -r '.Parameter.Value')
+    echo "$P"
+}
+
+BUCKET=$(get_param "/mc/backup-bucket")
+AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+EC2_NAME=$(/usr/local/bin/aws ec2 describe-tags --region $AZ --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
+
+# Save MC World
+/usr/local/bin/mcrcon -p test "say Saving world...." save-all
+filename=mc.$EC2_NAME.zip
+
+(cd /home/ubuntu/server && zip -r - world/) > $filename
+/usr/local/bin/aws s3 mv $filename s3://$BUCKET/worlds/
+
+/usr/local/bin/mcrcon -p test "say Backup saved to S3 bucket"
---
+++ ansible/roles/common/files/stop-check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+AZ=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r)
+PLAYERCOUNT=$(/usr/local/bin/mcrcon -H 127.0.0.1 -P 25575 -p test "list" | cut -d ' ' -f 3)
+AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+EC2_NAME=$(/usr/local/bin/aws ec2 describe-tags --region $AZ --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
+SNS_TOPIC=$(/usr/local/bin/aws ssm get-parameter --name "/mc/alert-sns" --with-decryption --region $AZ  | jq -r '.Parameter.Value')
+
+echo "Player count: $PLAYERCOUNT"
+if [ "$PLAYERCOUNT" -eq "0" ]; then
+
+        # Run save and backup
+        /home/ubuntu/scripts/backup-world.sh
+        /usr/local/bin/mcrcon -p test "say Stoping the server...." stop
+
+        /usr/local/bin/aws sns publish --region $AZ --topic-arn "${SNS_TOPIC}" --message "Shutting down ${EC2_NAME} due to inactivity."
+
+        sudo shutdown -h now
+        echo "Shutting down ${EC2_NAME} due to inactivity."
+else
+        echo "No shuttoff ${EC2_NAME}"
+fi
