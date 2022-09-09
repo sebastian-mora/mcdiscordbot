5f19d52c5733236f488320ea799535e0aa1e2d13
seb <seb@ruse.tech>
2022-09-09 00:32:39 -0700

    remove old resources

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -22,19 +22,3 @@ resource "aws_s3_bucket_versioning" "versioning_configuration" {
     status = "Enabled"
   }
 }
-
-resource "aws_s3_object" "upload-scripts" {
-  for_each = fileset("./resources/scripts", "*")
-  bucket   = aws_s3_bucket.mc-worlds.id
-  key      = "/scripts/${each.value}"
-  source   = "./resources/scripts/${each.value}"
-  etag     = filemd5("./resources/scripts/${each.value}")
-}
-
-resource "aws_s3_object" "upload-configs" {
-  for_each = fileset("./resources/server-configs", "**/*")
-  bucket   = aws_s3_bucket.mc-worlds.id
-  key      = "/server-configs/${each.value}"
-  source   = "./resources/server-configs/${each.value}"
-  etag     = filemd5("./resources/server-configs/${each.value}")
-}
