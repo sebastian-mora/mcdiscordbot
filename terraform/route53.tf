ff6e8182417e865667ec7b5bfe880aeb9d201291
seb <seb@ruse.tech>
2022-09-09 12:24:13 -0700

    remove modded

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -11,10 +11,10 @@ resource "aws_route53_record" "vanilla_mc_rusecrew_com" {
   records = [aws_instance.vanilla.public_ip]
 }
 
-resource "aws_route53_record" "modded_mc_rusecrew_com" {
-  zone_id = data.aws_route53_zone.primary.zone_id
-  name    = format("modded.%s", var.domain_name)
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.modded.public_ip]
-}
+# resource "aws_route53_record" "modded_mc_rusecrew_com" {
+#   zone_id = data.aws_route53_zone.primary.zone_id
+#   name    = format("modded.%s", var.domain_name)
+#   type    = "A"
+#   ttl     = 300
+#   records = [aws_instance.modded.public_ip]
+# }
