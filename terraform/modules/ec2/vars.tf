4dabc5309a5420d5d77555ceac45c8e2b91350cd
seb <seb@ruse.tech>
2022-09-09 13:34:48 -0700

    fmt

--- terraform/modules/ec2/vars.tf
+++ terraform/modules/ec2/vars.tf
@@ -1,23 +1,23 @@
 variable "name" {
-  type  = string
+  type = string
 }
 
 variable "instance_type" {
-  type = string
+  type    = string
   default = "m5.large"
 }
 
 variable "description" {
-  type  = string
+  type    = string
   default = "minecraft server"
 }
 
 variable "ansible_host_name" {
-  type  = string
+  type = string
 }
 
 variable "domain_name" {
-  type = string
- default     = "mc.rusecrew.com"
+  type    = string
+  default = "mc.rusecrew.com"
 }
 
