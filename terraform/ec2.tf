027d434e5c2430c0a6572382038adaa65d7d394a
seb <seb@ruse.tech>
2022-09-10 08:43:39 -0700

    all rl craft

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,3 +8,13 @@ module "vanilla" {
   subnet_id             = module.vpc.public_subnets[0]
   security_group_ids    = [aws_security_group.mc_sg.id, aws_security_group.allow_ssh_public.id]
 }
+
+module "rlcraft" {
+  source                = "./modules/ec2"
+  name                  = "rlcraft"
+  ansible_host_name     = "rlcraft"
+  instance_type         = "m5.large"
+  instance_profile_name = aws_iam_role.minecraft_server_role.name
+  subnet_id             = module.vpc.public_subnets[0]
+  security_group_ids    = [aws_security_group.mc_sg.id, aws_security_group.allow_ssh_public.id]
+}
