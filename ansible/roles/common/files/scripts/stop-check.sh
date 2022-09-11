1b6d3d0100a48e35452a1cbbc2fbdb91e8a95071
seb <seb@ruse.tech>
2022-09-11 12:31:31 -0700

    fix script

--- ansible/roles/common/files/scripts/stop-check.sh
+++ ansible/roles/common/files/scripts/stop-check.sh
@@ -3,14 +3,14 @@ get_param() {
     P=$(/usr/local/bin/aws ssm get-parameter --with-decryption --name  "$1" | jq -r '.Parameter.Value')
     echo "$P"
 }
-
+RCONPASS=$(get_param "/mc/rconpass")
 AZ=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r)
 # the last pipe allows this command to work on 1.12
 PLAYERCOUNT=$(/usr/local/bin/mcrcon -H 127.0.0.1 -P 25575 -p "$RCONPASS" "list" | cut -d ' ' -f 3 | cut -d '/' -f 1)
 AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 EC2_NAME=$(/usr/local/bin/aws ec2 describe-tags --region $AZ --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
 SNS_TOPIC=$(get_param "/mc/alert-sns")
-RCONPASS=$(get_param "/mc/rconpass")
+
 
 echo "Player count: $PLAYERCOUNT"
 if [ "$PLAYERCOUNT" -eq "0" ]; then
