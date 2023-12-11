119a93b31f6a3b22f417177b2e822f07fe290347
seb <seb@ruse.tech>
2023-12-11 10:54:03 -0500

    host only vanilla

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -13,25 +13,3 @@ module "vanilla" {
   subnet_id             = module.vpc.public_subnets[0]
   security_group_ids    = [aws_security_group.mc_sg.id, aws_security_group.allow_ssh_public.id]
 }
-
-module "rlcraft" {
-  source                = "./modules/ec2"
-  name                  = "rlcraft"
-  ansible_host_name     = "rlcraft"
-  instance_type         = "m5.large"
-  aws_key_pair_name     = aws_key_pair.deployer.key_name
-  instance_profile_name = aws_iam_role.minecraft_server_role.name
-  subnet_id             = module.vpc.public_subnets[0]
-  security_group_ids    = [aws_security_group.mc_sg.id, aws_security_group.allow_ssh_public.id]
-}
-
-module "skcraft4" {
-  source                = "./modules/ec2"
-  name                  = "skycraft4"
-  ansible_host_name     = "skycraft4"
-  instance_type         = "m5.large"
-  aws_key_pair_name     = aws_key_pair.deployer.key_name
-  instance_profile_name = aws_iam_role.minecraft_server_role.name
-  subnet_id             = module.vpc.public_subnets[0]
-  security_group_ids    = [aws_security_group.mc_sg.id, aws_security_group.allow_ssh_public.id]
-}
