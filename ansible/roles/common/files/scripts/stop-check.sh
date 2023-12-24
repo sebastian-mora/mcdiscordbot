addd9adca0c30320dbf830fff4217a6521f8c827
seb <seb@ruse.tech>
2023-12-23 21:58:04 -0500

    update shutdown

--- ansible/roles/common/files/scripts/stop-check.sh
+++ ansible/roles/common/files/scripts/stop-check.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
+
 get_param() {
-    P=$(/usr/local/bin/aws ssm get-parameter --with-decryption --name  "$1" | jq -r '.Parameter.Value')
+    P=$(/usr/local/bin/aws ssm get-parameter --with-decryption --name "$1" | jq -r '.Parameter.Value')
     echo "$P"
 }
+
 RCONPASS=$(get_param "/mc/rconpass")
 AZ=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r)
-# the last pipe allows this command to work on 1.12
-PLAYERCOUNT=$(/usr/local/bin/mcrcon -H 127.0.0.1 -P 25575 -p "$RCONPASS" "list" | cut -d ' ' -f 3 | cut -d '/' -f 1)
+
+PLAYERCOUNT=$(/usr/local/bin/mcrcon -H 127.0.0.1 -P 25575 -p "$RCONPASS" "list" | grep -oP 'There are \K\d+' || echo "Error getting player count")
+
 AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-EC2_NAME=$(/usr/local/bin/aws ec2 describe-tags --region $AZ --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
+EC2_NAME=$(/usr/local/bin/aws ec2 describe-tags --region "$AZ" --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
 SNS_TOPIC=$(get_param "/mc/alert-sns")
 
-
 echo "Player count: $PLAYERCOUNT"
+
 if [ "$PLAYERCOUNT" -eq "0" ]; then
+    # Run save and backup
+    /home/ubuntu/scripts/backup-world.sh
+    /usr/local/bin/mcrcon -p "$RCONPASS" "say Stopping the server...." stop
 
-        # Run save and backup
-        /home/ubuntu/scripts/backup-world.sh
-        /usr/local/bin/mcrcon -p "$RCONPASS" "say Stoping the server...." stop
-
-        /usr/local/bin/aws sns publish --region $AZ --topic-arn "${SNS_TOPIC}" --message "Shutting down ${EC2_NAME} due to inactivity."
-
+    if [ $? -eq 0 ]; then
+        sleep 5  # Add a 5-second delay
+        /usr/local/bin/aws sns publish --region "$AZ" --topic-arn "${SNS_TOPIC}" --message "Shutting down ${EC2_NAME} due to inactivity."
         sudo shutdown -h now
         echo "Shutting down ${EC2_NAME} due to inactivity."
+    else
+        echo "Error stopping the server."
+    fi
 else
-        echo "No shuttoff ${EC2_NAME}"
+    echo "No shutdown ${EC2_NAME}"
 fi
