a89650b824560b80515249e4e37a5da36a75987d
seb <seb@ruse.tech>
2023-12-25 00:54:19 -0500

    add prodigium

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -15,13 +15,26 @@ module "vanilla" {
   security_group_ids    = [aws_security_group.mc_sg.id, aws_security_group.allow_ssh_public.id]
 }
 
-module "chocolate" {
+# module "chocolate" {
+#   source                = "./modules/ec2"
+#   name                  = "chocolate"
+#   ansible_host_name     = "chocolate"
+#   instance_type         = "m5a.xlarge"
+#   disk_size             = 20 # 20GB
+#   description           = "Chocolate MC Version: 1.4.3"
+#   aws_key_pair_name     = aws_key_pair.deployer.key_name
+#   instance_profile_name = aws_iam_role.minecraft_server_role.name
+#   subnet_id             = module.vpc.public_subnets[0]
+#   security_group_ids    = [aws_security_group.mc_sg.id, aws_security_group.allow_ssh_public.id]
+# }
+
+module "prodigium" {
   source                = "./modules/ec2"
-  name                  = "chocolate"
-  ansible_host_name     = "chocolate"
+  name                  = "prodigium"
+  ansible_host_name     = "prodigium"
   instance_type         = "m5a.xlarge"
   disk_size             = 20 # 20GB
-  description           = "Chocolate MC Version: 1.4.3"
+  description           = "Prodigium MC Version: 3.1.6"
   aws_key_pair_name     = aws_key_pair.deployer.key_name
   instance_profile_name = aws_iam_role.minecraft_server_role.name
   subnet_id             = module.vpc.public_subnets[0]
